5a4b1a4d7cb5c255e0fac486e42c5f7365152576
Alexander Paulsell <alexander.dean.paulsell@gmail.com>
2019-06-21 18:09:34 -0700

    my packages on code did not notify me

--- src/main/java/data/document/document/DocumentApplication.java
+++ src/main/java/data/document/document/DocumentApplication.java
@@ -2,6 +2,8 @@ package data.document.document;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
 @Controller
 @SpringBootApplication
